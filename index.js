cff4398b1c71ebf9c53ddf4a546e0c767e937991
Prantosh <bepariprantosh360@gmail.com>
2021-11-16 23:51:16 +0600

    add doctor-portal

---
+++ index.js
@@ -0,0 +1,195 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const admin = require("firebase-admin"); //
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require("express-fileUpload");
+
+const port = process.env.PORT || 5000;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); //
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+}); //
+
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.vh8a9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function verifyToken(req, res, next) {
+  console.log(req.headers.authorization.startsWith("Bearer "));
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const token = req.headers.authorization.split(" ")[1];
+
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.dicodedEmail = decodedUser.email;
+    } catch {}
+  }
+  next();
+}
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("doctors_portal");
+    const appointmentsCollection = database.collection("appointments");
+    const usersCollection = database.collection("users");
+    const doctorsCollection = database.collection("doctors");
+
+    // all data pate app.get korte hoy
+    // email dia query kore single user air data ber korte app.get korte hoy
+    app.get("/appointments", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const date = req.query.date;
+
+      const query = { email: email, date: date };
+
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
+
+    // get appointment id for database
+    app.get("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await appointmentsCollection.findOne(query);
+      res.json(result);
+    });
+
+    // single data update korte app.post korte hoy
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      res.json(result);
+    });
+
+    app.put("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          payment: payment,
+        },
+      };
+      const result = await appointmentsCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    // get all doctors
+    app.get('/doctors', async (req, res) => {
+      const cursor = doctorsCollection.find({});
+      const doctor = await cursor.toArray();
+      res.json(doctor);
+    })
+
+    // add a doctor name email and image
+    app.post("/doctors", async (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const pic = req.files.image;
+      const picData = pic.data;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+      const doctor = {
+        name: name,
+        email: email,
+        image: imageBuffer,
+      };
+      const result = await doctorsCollection.insertOne(doctor);
+      res.json(result);
+    });
+
+    // varify admin for databasepoint
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // set the database single user email an name
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    // google email update database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { emil: user.emil };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // add user admin
+    app.put("/users/admin", verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.dicodedEmail;
+      if (requester) {
+        const requesterAccount = await usersCollection.findOne({
+          email: requester,
+        });
+        if (requesterAccount.role === "admin") {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        }
+      } else {
+        res
+          .status(403)
+          .json({ message: "you do not have an access to make admin" });
+      }
+    });
+
+    //  pament mathod
+    app.post("/create-payment-intent", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = paymentInfo.price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      console.log("pamentIntent", paymentIntent);
+      res.json({ clientSecret: paymentIntent.client_secret });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome doctors portal");
+});
+
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
